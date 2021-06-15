8a4d3927253b1313c0cd3ea00f95f7d9eace4f35
HERO5151 <pixy.gtr@gmail.com>
2021-06-15 21:34:47 +0900

    ・mysqlコンテナ設置 ・DB情報を環境変数で定義するためdotenv追加 ・mysql接続ドライバ追加 ・mysqlの接続と取得が確認できるまで作業

--- server/index.js
+++ server/index.js
@@ -3,8 +3,23 @@ const { loadNuxt, build } = require('nuxt')
 const app = require('express')()
 const isDev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
+require('dotenv').config()
 
 async function start() {
+  const mysql = require('mysql2');
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  });
+
+  connection.query('select * from users;', function(err, users) {
+    console.log(users);
+  });
+
+  return;
+
   // We get Nuxt instance
   const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
 
